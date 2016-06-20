97be6598047aa3b0a17917a6dfb7293a61d588eb
Eli Sadoff <snood1205@gmail.com>
2016-06-20 18:59:45 -0400

    Finally got word count on Rust

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,15 +1,22 @@
 extern crate regex;
 use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 
 pub fn word_count(s: &str) -> HashMap<String, u32> {
-  let re = regex::Regex::new(r"[^A-Za-z0-9\w'").unwrap();
+  if s == "" {
+    return HashMap::new();
+  }
+  let re = regex::Regex::new(r"[^A-Za-z0-9]+").unwrap();
   let s = re.split(s).collect::<Vec<&str>>();
   let mut h: HashMap<String, u32> = HashMap::new();
   for x in s {
-    match h.get(x) {
-      Some(i) => h.entry(x.to_string()).or_insert_with(i + 1),
-      None    => h.entry(x.to_string()).or_insert_with(1)
+    if x == "" {
+      continue;
     }
-  } 
-  h;
+    match h.entry(x.to_string().to_lowercase()) {
+      Entry::Vacant(v) => {v.insert(1);},
+      Entry::Occupied(o) => {*o.into_mut() += 1;},
+    }
+  }
+  h
 }
