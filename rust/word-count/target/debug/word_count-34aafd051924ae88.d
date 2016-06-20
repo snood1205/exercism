1c8bd62308dd6a7ab5efa4b99446753d8eb5ddc9
Eli Sadoff <snood1205@gmail.com>
2016-06-20 14:31:04 -0400

    Removed build

--- rust/word-count/target/debug/word_count-34aafd051924ae88.d
+++
@@ -1,5 +0,0 @@
-/Users/eli/exercism/rust/word-count/target/debug/word_count-34aafd051924ae88: src/lib.rs
-
-/Users/eli/exercism/rust/word-count/target/debug/word_count-34aafd051924ae88.d: src/lib.rs
-
-src/lib.rs:
--- rust/word-count/target/debug/word_count.d
+++
@@ -1,5 +0,0 @@
-/Users/eli/exercism/rust/word-count/target/debug/libword_count.rlib: src/lib.rs
-
-/Users/eli/exercism/rust/word-count/target/debug/word_count.d: src/lib.rs
-
-src/lib.rs:
